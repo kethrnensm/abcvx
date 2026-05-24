0d830feac75f1e52bbfc4134a581299351235bba
mainthay963-design <mainthay963@gmail.com>
2026-05-24 12:34:57 +0700

    Update dialog.cpp

--- client/jni/dialog.cpp
+++ client/jni/dialog.cpp
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <sstream>
 #include <iostream>
+#include <vector>
 
 extern CGUI* pGUI;
 extern CGame* pGame;
@@ -20,12 +21,21 @@ extern CScoreBoard* pScoreBoard;
 char szDialogInputBuffer[100];
 char utf8DialogInputBuffer[100 * 3];
 
+static float GetKeyboardOffset()
+{
+	if (pKeyBoard && pKeyBoard->IsOpen() && pGUI)
+		return pGUI->ScaleY(320.0f);
+
+	return 0.0f;
+}
+
 CDialogWindow::CDialogWindow()
 {
 	m_bIsActive = false;
 	m_putf8Info = nullptr;
 	m_pszInfo = nullptr;
 }
+
 ImVec2 CalcTextSizeWithoutTags(char* szStr)
 {
 	if (!szStr) return ImVec2(0, 0);
@@ -33,9 +43,9 @@ ImVec2 CalcTextSizeWithoutTags(char* szStr)
 	char szNonColored[2048 + 1];
 	int iNonColoredMsgLen = 0;
 
-	for (int pos = 0; pos < strlen(szStr) && szStr[pos] != '\0'; pos++)
+	for (int pos = 0; pos < (int)strlen(szStr) && szStr[pos] != '\0'; pos++)
 	{
-		if (pos + 7 < strlen(szStr))
+		if (pos + 7 < (int)strlen(szStr))
 		{
 			if (szStr[pos] == '{' && szStr[pos + 7] == '}')
 			{
@@ -55,7 +65,6 @@ ImVec2 CalcTextSizeWithoutTags(char* szStr)
 
 CDialogWindow::~CDialogWindow()
 {
-
 }
 
 void CDialogWindow::Show(bool bShow)
@@ -64,7 +73,7 @@ void CDialogWindow::Show(bool bShow)
 	{
 		if (pGame && pGame->FindPlayerPed()
 			&& !pNetGame->GetTextDrawPool()->m_bSelectState)
-			//if(!pGame->FindPlayerPed()->GetVehicleSeatID()) 
+			//if(!pGame->FindPlayerPed()->GetVehicleSeatID())
 			pGame->FindPlayerPed()->TogglePlayerControllable(!bShow);
 	}*/
 
@@ -96,8 +105,8 @@ void CDialogWindow::Clear()
 	m_fSizeX = -1.0f;
 	m_fSizeY = -1.0f;
 
-	memset(szDialogInputBuffer, 0, 100);
-	memset(utf8DialogInputBuffer, 0, 100 * 3);
+	memset(szDialogInputBuffer, 0, sizeof(szDialogInputBuffer));
+	memset(utf8DialogInputBuffer, 0, sizeof(utf8DialogInputBuffer));
 }
 
 void CDialogWindow::SetInfo(char* szInfo, int length)
@@ -176,21 +185,18 @@ void TextWithColors(const char* fmt, ...)
 	{
 		if (*textCur == '{')
 		{
-			// Print accumulated text
 			if (textCur != textStart)
 			{
 				ImGui::TextUnformatted(textStart, textCur);
 				ImGui::SameLine(0.0f, 0.0f);
 			}
 
-			// Process color code
 			const char* colorStart = textCur + 1;
 			do
 			{
 				++textCur;
 			} while (*textCur != '\0' && *textCur != '}');
 
-			// Change color
 			if (pushedColorStyle)
 			{
 				ImGui::PopStyleColor();
@@ -208,7 +214,6 @@ void TextWithColors(const char* fmt, ...)
 		}
 		else if (*textCur == '\n')
 		{
-			// Print accumulated text an go to next line
 			ImGui::TextUnformatted(textStart, textCur);
 			textStart = textCur + 1;
 		}
@@ -251,19 +256,20 @@ std::string removeColorTags(std::string line)
 	}
 	return string;
 }
+
 #include "chatwindow.h"
 extern CChatWindow* pChatWindow;
 #include "CServerManager.h"
 #include "CSettings.h"
 extern CSettings* pSettings;
+
 void CDialogWindow::RenderTabList(int dStyle)
 {
-	std::string strUtf8 = m_putf8Info;
-	int size = strUtf8.length();
+	std::string strUtf8 = m_putf8Info ? m_putf8Info : "";
 	int iTabsCount = 0;
 	std::vector<std::string> vLines;
 	std::vector<std::string> firstTabs;
-	ImVec2 fTabSize[4];
+	ImVec2 fTabSize[4] = {};
 
 	int tmplineid = 0;
 	std::stringstream ssLine(strUtf8);
@@ -278,28 +284,26 @@ void CDialogWindow::RenderTabList(int dStyle)
 			std::string tmpTabLine;
 			while (std::getline(ssTabLine, tmpTabLine, '\t'))
 			{
-				if (tmpTabId >= 4) continue;
+				if (tmpTabId >= 4) break;
+
 				if (vLines.size() == 1 && iTabsCount <= 4)
 					iTabsCount++;
-				ImVec2 tabSize;
-				tabSize = CalcTextSizeWithoutTags((char*)tmpTabLine.c_str());
+
+				ImVec2 tabSize = CalcTextSizeWithoutTags((char*)tmpTabLine.c_str());
 
 				if (tmpTabId == 0)
 					firstTabs.push_back(removeColorTags(tmpTabLine));
-
 
 				if (tabSize.x > fTabSize[tmpTabId].x)
 				{
 					fTabSize[tmpTabId].x = pGUI->ScaleX(tabSize.x);
 				}
 				tmpTabId++;
-
 			}
 		}
 		else
 		{
-			ImVec2 tabSize;
-			tabSize = CalcTextSizeWithoutTags((char*)tmpLine.c_str());
+			ImVec2 tabSize = CalcTextSizeWithoutTags((char*)tmpLine.c_str());
 
 			if (tmpTabId == 0)
 				firstTabs.push_back(tmpLine);
@@ -311,41 +315,49 @@ void CDialogWindow::RenderTabList(int dStyle)
 		}
 		tmplineid++;
 	}
+
 	if (iTabsCount == 1 && dStyle == DIALOG_STYLE_TABLIST)
 		dStyle = DIALOG_STYLE_LIST;
 
 	ImGuiIO& io = ImGui::GetIO();
-	int x = io.DisplaySize.x, y = io.DisplaySize.y;
 
 	float buttonFactor = pGUI->ScaleX(187.5f) * 2 + pGUI->GetFontSize();
 
-	ImVec2 vecWinSize;
+	ImVec2 vecWinSize(0.0f, 720.0f);
+
 	if (dStyle != DIALOG_STYLE_LIST)
 	{
 		for (uint8_t i = 0; i < iTabsCount; i++)
 		{
-			vecWinSize.x += fTabSize[i].x * (i == iTabsCount - 1 ? 1.25 : 1.5);
+			vecWinSize.x += fTabSize[i].x * (i == iTabsCount - 1 ? 1.25f : 1.5f);
 		}
 	}
-	else vecWinSize.x += fTabSize[0].x * 1.25;
+	else
+	{
+		vecWinSize.x += fTabSize[0].x * 1.25f;
+	}
+
 	if (buttonFactor > vecWinSize.x)
 	{
 		vecWinSize.x = 0.0f;
 
 		if (dStyle != DIALOG_STYLE_LIST)
 		{
-			buttonFactor /= iTabsCount;
+			if (iTabsCount > 0)
+				buttonFactor /= iTabsCount;
+
 			for (uint8_t i = 0; i < iTabsCount; i++)
 			{
 				vecWinSize.x += fTabSize[i].x + buttonFactor;
 			}
 		}
-		else vecWinSize.x += fTabSize[0].x + buttonFactor;
+		else
+			vecWinSize.x += fTabSize[0].x + buttonFactor;
 	}
 	else
-		buttonFactor = 0;
-
-	vecWinSize.y = 720.0f;
+	{
+		buttonFactor = 0.0f;
+	}
 
 	if (vLines.size() > 7)
 		vecWinSize.x += ImGui::GetStyle().ScrollbarSize;
@@ -356,28 +368,46 @@ void CDialogWindow::RenderTabList(int dStyle)
 	if (vecWinSize.x > pGUI->ScaleX(1800.0f))
 		vecWinSize.x = pGUI->ScaleX(1800.0f);
 
-	ImGui::Begin(" ", nullptr, ImVec2(pGUI->ScaleX(vecWinSize.x + pGUI->GetFontSize()), pGUI->ScaleY(vecWinSize.y)), 0.9f, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize); //
-	
+	ImVec2 winSize(
+		pGUI->ScaleX(vecWinSize.x + pGUI->GetFontSize()),
+		pGUI->ScaleY(vecWinSize.y) + (dStyle == DIALOG_STYLE_TABLIST_HEADERS ? pGUI->ScaleY(187.5f) / 2.0f : 0.0f)
+	);
+
+	float keyboardOffset = GetKeyboardOffset();
+	float posX = ((io.DisplaySize.x - winSize.x) / 2.0f);
+	float posY = ((io.DisplaySize.y - winSize.y - keyboardOffset) / 2.0f);
+
+	if (posY < 10.0f)
+		posY = 10.0f;
+
+	ImGui::SetNextWindowPos(ImVec2(posX, posY));
+	ImGui::SetNextWindowSize(winSize);
+
+	ImGui::Begin(" ", nullptr, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+
 	m_bRendered = true;
 	TextWithColors(m_utf8Title);
 	ImGui::ItemSize(ImVec2(0, pGUI->GetFontSize() / 2 + 2.75f));
 
 	ImVec2 cursonPosition;
 	cursonPosition = ImGui::GetCursorPos();
+
 	if (dStyle == DIALOG_STYLE_TABLIST_HEADERS)
 	{
 		ImGui::Columns(iTabsCount, OBFUSCATE("###tablistHeader"), false);
 		for (uint16_t i = 0; i < iTabsCount; i++)
 		{
-			ImGui::SetColumnWidth(-1, fTabSize[i].x * (i == iTabsCount - 1 ? 1.25 : 1.5) + buttonFactor);
+			ImGui::SetColumnWidth(-1, fTabSize[i].x * (i == iTabsCount - 1 ? 1.25f : 1.5f) + buttonFactor);
 			ImGui::NextColumn();
 		}
+
 		std::stringstream ssTabLine(vLines[0]);
 		std::string tmpTabLine;
 		int tmpTabId = 0;
 		while (std::getline(ssTabLine, tmpTabLine, '\t'))
 		{
 			if (tmpTabId > iTabsCount) continue;
+
 			tmpTabLine.insert(0, OBFUSCATE("{a9c4e4}"));
 			if (tmpTabId == 0)
 			{
@@ -387,7 +417,6 @@ void CDialogWindow::RenderTabList(int dStyle)
 			TextWithColors(tmpTabLine.c_str());
 			ImGui::NextColumn();
 			tmpTabId++;
-
 		}
 		ImGui::Columns(1);
 	}
@@ -396,7 +425,7 @@ void CDialogWindow::RenderTabList(int dStyle)
 
 	if (vecWinSize.x > pGUI->ScaleX(1800.0f))
 	{
-		for (auto & tabSize : fTabSize)
+		for (auto& tabSize : fTabSize)
 			tsize += tabSize.x;
 
 		tsize -= ImGui::GetStyle().ScrollbarSize;
@@ -410,7 +439,7 @@ void CDialogWindow::RenderTabList(int dStyle)
 		ImGui::Columns(iTabsCount, OBFUSCATE("###tablistColumn"), false);
 		for (uint16_t i = 0; i < iTabsCount; i++)
 		{
-			ImGui::SetColumnWidth(-1, fTabSize[i].x * (i == iTabsCount - 1 ? 1.25 : 1.5) + buttonFactor);
+			ImGui::SetColumnWidth(-1, fTabSize[i].x * (i == iTabsCount - 1 ? 1.25f : 1.5f) + buttonFactor);
 			ImGui::NextColumn();
 		}
 	}
@@ -421,7 +450,8 @@ void CDialogWindow::RenderTabList(int dStyle)
 		std::string tmpTabLine;
 		int tmpTabId = 0;
 
-		ImVec2 differentOffsets;
+		ImVec2 differentOffsets = ImVec2(0.0f, 0.0f);
+
 		if (tmpTabId == 0)
 		{
 			ImGui::GetStyle().Colors[ImGuiCol_HeaderActive] = (ImVec4)ImColor(119, 4, 4, 255);
@@ -446,7 +476,7 @@ void CDialogWindow::RenderTabList(int dStyle)
 					Show(false);
 					if (pNetGame)
 					{
-						if(dStyle == DIALOG_STYLE_TABLIST_HEADERS) m_iSelectedItem--;
+						if (dStyle == DIALOG_STYLE_TABLIST_HEADERS) m_iSelectedItem--;
 						pNetGame->SendDialogResponse(m_wDialogID, 1, m_iSelectedItem, (char*)firstTabs[m_iSelectedItem].c_str());
 					}
 					else
@@ -457,23 +487,25 @@ void CDialogWindow::RenderTabList(int dStyle)
 							pSettings->GetReadOnly().szNickName,
 							pSettings->GetReadOnly().szPassword);
 					}
-
 				}
+
 			if (ImGui::IsItemHovered())
 			{
 				m_iSelectedItem = line;
 			}
+
 			ss.clear();
 			ss << line + tmpTabId + 1;
 			s = ss.str();
 			itemid = "##" + s;
+
 			if (ImGui::Selectable(itemid.c_str(), is_selected, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowDoubleClick))
 				if (ImGui::IsMouseDoubleClicked(0) && m_iSelectedItem == line)
 				{
 					Show(false);
 					if (pNetGame)
 					{
-						if(dStyle == DIALOG_STYLE_TABLIST_HEADERS) m_iSelectedItem--;
+						if (dStyle == DIALOG_STYLE_TABLIST_HEADERS) m_iSelectedItem--;
 						pNetGame->SendDialogResponse(m_wDialogID, 1, m_iSelectedItem, (char*)firstTabs[m_iSelectedItem].c_str());
 					}
 					else
@@ -484,13 +516,13 @@ void CDialogWindow::RenderTabList(int dStyle)
 							pSettings->GetReadOnly().szNickName,
 							pSettings->GetReadOnly().szPassword);
 					}
-
-
 				}
+
 			if (ImGui::IsItemHovered())
 			{
 				m_iSelectedItem = line;
 			}
+
 			ImVec2 cursonPositionLast;
 			cursonPositionLast = ImGui::GetCursorPos();
 
@@ -501,8 +533,8 @@ void CDialogWindow::RenderTabList(int dStyle)
 
 			if (dStyle != DIALOG_STYLE_LIST)
 				ImGui::SameLine();
-
 		}
+
 		if (dStyle != DIALOG_STYLE_LIST)
 		{
 			int icolumnsLeft = iTabsCount;
@@ -520,7 +552,9 @@ void CDialogWindow::RenderTabList(int dStyle)
 					ImGui::SetCursorPos(ImVec2(cursonPos.x, cursonPos.y - differentOffsets.y / 4));
 				}
 				else
+				{
 					ImGui::SetCursorPos(ImVec2(cursonPos.x, cursonPos.y + differentOffsets.y / 4));
+				}
 
 				tmpTabLine.insert(0, OBFUSCATE("{ffffff}"));
 				TextWithColors((char*)tmpTabLine.c_str());
@@ -532,8 +566,8 @@ void CDialogWindow::RenderTabList(int dStyle)
 					icolumnsLeft--;
 				}
 				tmpTabId++;
-
 			}
+
 			if (dStyle != DIALOG_STYLE_LIST && icolumnsLeft >= 0)
 			{
 				for (int i = 0; i < icolumnsLeft; i++)
@@ -549,12 +583,12 @@ void CDialogWindow::RenderTabList(int dStyle)
 
 			if (line == m_iSelectedItem)
 				m_strSelectedItemText = vLines[line];
+
 			ImGui::SetCursorPos(ImVec2(cursonPos.x, cursonPos.y - differentOffsets.y + pGUI->GetFontSize() / 2));
 
 			vLines[line].insert(0, OBFUSCATE("{ffffff}"));
 			TextWithColors((char*)vLines[line].c_str());
 			ImGui::SetCursorPos(ImVec2(cursonPos.x, cursonPos.y));
-
 		}
 	}
 
@@ -577,6 +611,7 @@ void CDialogWindow::RenderTabList(int dStyle)
 			if (dStyle == DIALOG_STYLE_TABLIST_HEADERS && !m_iSelectedItem)
 				TempItem = 0;
 			else TempItem = m_iSelectedItem - 1;
+
 			if (pNetGame)
 				pNetGame->SendDialogResponse(m_wDialogID, 1, dStyle == DIALOG_STYLE_TABLIST_HEADERS ? TempItem : m_iSelectedItem, (char*)firstTabs[m_iSelectedItem].c_str());
 			else
@@ -587,10 +622,11 @@ void CDialogWindow::RenderTabList(int dStyle)
 					pSettings->GetReadOnly().szNickName,
 					pSettings->GetReadOnly().szPassword);
 			}
-
 		}
 	}
+
 	ImGui::SameLine(0, pGUI->GetFontSize());
+
 	if (m_utf8Button2[0] != 0)
 	{
 		if (ImGui::Button(m_utf8Button2, ImVec2(pGUI->ScaleX(193.5f), pGUI->ScaleY(75.0f))))
@@ -600,6 +636,7 @@ void CDialogWindow::RenderTabList(int dStyle)
 			if (dStyle == DIALOG_STYLE_TABLIST_HEADERS && !m_iSelectedItem)
 				TempItem = 0;
 			else TempItem = m_iSelectedItem - 1;
+
 			if (pNetGame)
 				pNetGame->SendDialogResponse(m_wDialogID, 0, dStyle == DIALOG_STYLE_TABLIST_HEADERS ? TempItem : m_iSelectedItem, (char*)firstTabs[m_iSelectedItem].c_str());
 			else
@@ -612,23 +649,19 @@ void CDialogWindow::RenderTabList(int dStyle)
 			}
 		}
 	}
-	
-	if (vecWinSize.x < pGUI->ScaleX(800.0f))
-		vecWinSize.x = pGUI->ScaleX(800.0f);
 
-	if (vecWinSize.x > pGUI->ScaleX(1800.0f))
-		vecWinSize.x = pGUI->ScaleX(1800.0f);
+	// Nếu bạn vẫn muốn ép lại vị trí mỗi frame, để ở đây, nhưng đã có SetNextWindowPos trước Begin rồi.
+	// Mình để lại comment cho khỏi ai đó lỡ tay cắt rồi lại quay về cảnh bàn phím nuốt dialog.
+	ImVec2 winSizeFinal = ImGui::GetWindowSize();
+	float keyboardOffsetFinal = GetKeyboardOffset();
+	float posXFinal = ((io.DisplaySize.x - winSizeFinal.x) / 2.0f);
+	float posYFinal = ((io.DisplaySize.y - winSizeFinal.y - keyboardOffsetFinal) / 2.0f);
 
-	ImVec2 winSize(vecWinSize.x, pGUI->ScaleY(vecWinSize.y + dStyle == DIALOG_STYLE_TABLIST_HEADERS ? pGUI->ScaleY(187.5f) / 2 : 0));
-	ImGui::SetWindowSize(winSize);
-	ImGui::SetWindowPos(ImVec2(((io.DisplaySize.x - winSize.x) / 2), ((io.DisplaySize.y - winSize.y) / 2)));
-	//
+	if (posYFinal < 10.0f)
+		posYFinal = 10.0f;
 
-	/*if (pGUI->ScaleX(ImGui::GetWindowWidth()) > 1800.0f)
-		ImGui::SetWindowSize(ImVec2(pGUI->ScaleX(1800.0f), ImGui::GetWindowHeight()));*/
-
+	ImGui::SetWindowPos(ImVec2(posXFinal, posYFinal));
 	ImGui::End();
-
 }
 
 void CDialogWindow::Render()
@@ -638,13 +671,11 @@ void CDialogWindow::Render()
 
 	ImGuiIO& io = ImGui::GetIO();
 
-	ImGui::SetNextWindowSize(ImVec2(0, 0));
-	ImGui::SetNextWindowPosCenter();
-
 	if (m_byteDialogStyle == DIALOG_STYLE_TABLIST_HEADERS || m_byteDialogStyle == DIALOG_STYLE_TABLIST || m_byteDialogStyle == DIALOG_STYLE_LIST)
 		return RenderTabList(m_byteDialogStyle);
 
-	//ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(pGUI->GetFontSize(), pGUI->GetFontSize()));
+	ImGui::SetNextWindowSize(ImVec2(0, 0));
+	ImGui::SetNextWindowPosCenter();
 
 	ImGui::Begin(" ", nullptr,
 		ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
@@ -677,10 +708,11 @@ void CDialogWindow::Render()
 	case DIALOG_STYLE_PASSWORD:
 		TextWithColors(m_putf8Info);
 		ImGui::ItemSize(ImVec2(0, pGUI->GetFontSize() / 2 + 10));
+
 		char _utf8DialogInputBuffer[100 * 3 + 1];
 		strcpy(_utf8DialogInputBuffer, utf8DialogInputBuffer);
 
-		for (int i = 0; i < strlen(_utf8DialogInputBuffer); i++)
+		for (int i = 0; i < (int)strlen(_utf8DialogInputBuffer); i++)
 		{
 			if (_utf8DialogInputBuffer[i] == '\0')
 				break;
@@ -710,7 +742,9 @@ void CDialogWindow::Render()
 				pNetGame->SendDialogResponse(m_wDialogID, 1, 0, szDialogInputBuffer);
 		}
 	}
+
 	ImGui::SameLine(0, pGUI->GetFontSize());
+
 	if (m_utf8Button2[0] != 0)
 	{
 		if (ImGui::Button(m_utf8Button2, ImVec2(pGUI->ScaleX(187.5f), pGUI->ScaleY(75.0f))))
@@ -722,7 +756,13 @@ void CDialogWindow::Render()
 	}
 
 	ImVec2 size = ImGui::GetWindowSize();
-	ImGui::SetWindowPos(ImVec2(((io.DisplaySize.x - size.x) / 2), ((io.DisplaySize.y - size.y) / 2)));
-	ImGui::End();
+	float keyboardOffset = GetKeyboardOffset();
+	float posX = ((io.DisplaySize.x - size.x) / 2.0f);
+	float posY = ((io.DisplaySize.y - size.y - keyboardOffset) / 2.0f);
 
+	if (posY < 10.0f)
+		posY = 10.0f;
+
+	ImGui::SetWindowPos(ImVec2(posX, posY));
+	ImGui::End();
 }
