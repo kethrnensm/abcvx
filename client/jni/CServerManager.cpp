7b2582117106cc21465d2cc5b607caa362fba9ea
kethrnensm <luongluongthihoa55@gmail.com>
2026-06-30 07:19:10 +0700

    Update CServerManager.cpp

--- client/jni/CServerManager.cpp
+++ client/jni/CServerManager.cpp
@@ -11,7 +11,7 @@ extern CDialogWindow *pDialogWindow;
 
 #ifdef FLIN
 	const CServerInstance::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
-		CServerInstance::create(OBFUSCATE("142.132.203.47"), 1, 20, 15163, true),
-		CServerInstance::create(OBFUSCATE("142.132.203.47"), 1, 20, 15163, true),
+		CServerInstance::create(OBFUSCATE("51.68.107.75"), 1, 20, 11616, true),
+		CServerInstance::create(OBFUSCATE("51.68.107.75"), 1, 20, 11616, true),
 	};
 #endif
